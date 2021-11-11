6cac82bcc30aa8ac4bdf4ad54944f76027e7a6fb
Jonathan Shnayder <rangersps42@gmail.com>
2021-11-11 10:03:39 -0500

    Added tests and Notes to README

--- main.cpp
+++ main.cpp
@@ -5,43 +5,6 @@
 
 int main()
 {
-  	Coord3D pointA = {10,20,30};
-	std::cout << length(&pointA) << std::endl;
-	
-	Coord3D pointP = {10, 20, 30};
-    Coord3D pointQ = {-20, 21, -22};
-
-    std::cout << "Address of P: " << &pointP << std::endl;
-    std::cout << "Address of Q: " << &pointQ << std::endl << std::endl;
-
-    Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
-   
-    std::cout << "ans = " << ans << std::endl; // So which point is farther?
-	
-	    
-	Coord3D pos = {0, 0, 100.0};
-    Coord3D vel = {1, -5, 0.2};
-
-    move(&pos, &vel, 2.0);
-	std::cout << pos.x << " " << pos.y << " " << pos.z << std::endl;
-
-	double x, y, z;
-	std::cout << "Enter position: ",
-	std::cin >> x >> y >> z;
-	Coord3D *ppos = createCoord3D(x,y,z);
-
-	std::cout << "Enter velocity: ";
-	std::cin >> x >> y >> z;
-	Coord3D *pvel = createCoord3D(x,y,z);
-
-	move(ppos, pvel, 10.0);
-	
-	 std::cout << "Coordinates after 10 seconds: " 
-         << (*ppos).x << " " << (*ppos).y << " " << (*ppos).z << std::endl;
-	
-	deleteCoord3D(ppos); // release memory
-    deleteCoord3D(pvel);
-	
 	std::cout << "-----------------------------------" << std::endl;
 
 	Particle *q= createParticle(1.0, 1.5, 2.0, 0.1, 0.2, 0.3);
--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,47 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "funcs.h"
+#include "coord3d.h"
+#include "cmath"
 
-// add your tests here
+// Only compared a certian amount of decimal spaces as the entire thing was not working
+TEST_CASE("Testing Task A Length") {
+	Coord3D pointP = {10, 20, 30};
+	double answer = int(length(&pointP) * 10000) / double(10000);
+	CHECK(answer == double(37.4165));
+
+	Coord3D pointX = {5, 8, 10};
+	answer = int(length(&pointX) * 10000) / double(10000);
+	CHECK(answer == double(13.7477));
+
+}
+
+TEST_CASE("Testing Task B fartherFromOrigin") {
+	Coord3D pointP = {10,20,30};
+	Coord3D pointQ = {-20, 21, -22};
+	Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
+	CHECK(ans == &pointP);
+
+	Coord3D pointA = {5, 10 ,30};
+	Coord3D pointB = {-5, -100, -20};
+	ans = fartherFromOrigin(&pointA, &pointB);
+	CHECK(ans == &pointB);
+}
+
+TEST_CASE("Testing Task C move") {
+	Coord3D posA = {0, 0, 100.0};
+	Coord3D velA = {1, -5, 0.2};
+	move(&posA, &velA, 2.0);
+	CHECK(posA.x == 2);
+	CHECK(posA.y == -10);
+	CHECK(posA.z == 100.4);
+
+	Coord3D posB = {5, 8, 50};
+	Coord3D velB = {-2, 10, .8};
+	move(&posB, &velB, 1.5);
+	CHECK(posB.x == 2);
+	CHECK(posB.y == 23);
+	CHECK(posB.z == 51.2);
+}
+
+
